847c60df0e2a12cdc4020f10278cba75187d1b49
mad <ma.dupuis.90@gmail.com>
2023-05-22 17:18:03 -0400

    progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'angular1/list',
+    component: ListComponent,
+  },
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: '**',
+    component: EmptyRouteComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/angular1' }]
 })
 export class AppRoutingModule { }
